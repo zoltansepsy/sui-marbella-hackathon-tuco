528f3e362ec0b9167a0cb94948dca2e1a2fcb830
Zoltan Sepsy <zoltan.sepsy@gmail.com>
2025-11-22 02:43:20 +0100

    Update devnet package addresses

--- app/constants.ts
+++ app/constants.ts
@@ -1,11 +1,11 @@
 // ======== Example Contracts ========
 
-export const DEVNET_COUNTER_PACKAGE_ID = "0xTODO";
-export const TESTNET_COUNTER_PACKAGE_ID = "0xcea82fb908b9d9566b1c7977491e76901ed167978a1ecd6053a994881c0ea9b5";
+export const DEVNET_COUNTER_PACKAGE_ID = "0xbce15267365bebfd0267e4789406ec89ba7901e687301da0d3fdb6d3e5be81fb";
+export const TESTNET_COUNTER_PACKAGE_ID = "0xTODO_AFTER_DEPLOY";
 export const MAINNET_COUNTER_PACKAGE_ID = "0xTODO";
 
-export const DEVNET_WHITELIST_PACKAGE_ID = "0x20a16ab8344765dccdbd4c87c5aabf4484cb69ec7822265fe3a37e3efd99eda5";
-export const TESTNET_WHITELIST_PACKAGE_ID = "0x0fb53a1ee7068ed47c21fe299c727a99c014a60fdcbefd9ca4bb2a76b66d7467";
+export const DEVNET_WHITELIST_PACKAGE_ID = "0xbce15267365bebfd0267e4789406ec89ba7901e687301da0d3fdb6d3e5be81fb";
+export const TESTNET_WHITELIST_PACKAGE_ID = "0xTODO_AFTER_DEPLOY";
 export const MAINNET_WHITELIST_PACKAGE_ID = "0xTODO";
 
 // ======== Freelance Platform Contracts ========
@@ -15,7 +15,7 @@ export const MAINNET_WHITELIST_PACKAGE_ID = "0xTODO";
  * Job Escrow Package IDs
  * Deploy with: cd move/zk_freelance && sui client publish --gas-budget 100000000 .
  */
-export const DEVNET_JOB_ESCROW_PACKAGE_ID = "0x20a16ab8344765dccdbd4c87c5aabf4484cb69ec7822265fe3a37e3efd99eda5";
+export const DEVNET_JOB_ESCROW_PACKAGE_ID = "0xbce15267365bebfd0267e4789406ec89ba7901e687301da0d3fdb6d3e5be81fb";
 export const TESTNET_JOB_ESCROW_PACKAGE_ID = "0xTODO_AFTER_DEPLOY";
 export const MAINNET_JOB_ESCROW_PACKAGE_ID = "0xTODO";
 
@@ -23,7 +23,7 @@ export const MAINNET_JOB_ESCROW_PACKAGE_ID = "0xTODO";
  * Profile NFT Package IDs
  * Uses same package as job_escrow (part of zk_freelance package)
  */
-export const DEVNET_PROFILE_NFT_PACKAGE_ID = "0x20a16ab8344765dccdbd4c87c5aabf4484cb69ec7822265fe3a37e3efd99eda5";
+export const DEVNET_PROFILE_NFT_PACKAGE_ID = "0xbce15267365bebfd0267e4789406ec89ba7901e687301da0d3fdb6d3e5be81fb";
 export const TESTNET_PROFILE_NFT_PACKAGE_ID = "0xTODO_AFTER_DEPLOY";
 export const MAINNET_PROFILE_NFT_PACKAGE_ID = "0xTODO";
 
@@ -31,7 +31,7 @@ export const MAINNET_PROFILE_NFT_PACKAGE_ID = "0xTODO";
  * Reputation Package IDs
  * Uses same package as job_escrow (part of zk_freelance package)
  */
-export const DEVNET_REPUTATION_PACKAGE_ID = "0x20a16ab8344765dccdbd4c87c5aabf4484cb69ec7822265fe3a37e3efd99eda5";
+export const DEVNET_REPUTATION_PACKAGE_ID = "0xbce15267365bebfd0267e4789406ec89ba7901e687301da0d3fdb6d3e5be81fb";
 export const TESTNET_REPUTATION_PACKAGE_ID = "0xTODO_AFTER_DEPLOY";
 export const MAINNET_REPUTATION_PACKAGE_ID = "0xTODO";
 
